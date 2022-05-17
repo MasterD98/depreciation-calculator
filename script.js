ddb4c10042aa9fd2bd7bf28bc83cb7e1a4627561
Dasith Rathnasinghe <dasithrathnasinghe@gmail.com>
2022-05-18 02:19:47 +0530

    style done

--- script.js
+++ script.js
@@ -1,46 +1,47 @@
-const form =document.getElementById('form');
-const methods=document.getElementById('methods');
-const cost=document.getElementById('cost');
-const usefulLife=document.getElementById('usefulLife');
-const scrapValue=document.getElementById('scrapValue');
-const accDepreciation=document.getElementById('accDepreciation');
-const scrap=document.getElementById('scrap');
-const noOfUnits=document.getElementById('noOfUnits');
-const productionPerYear= document.getElementById('productionPerYear');
-const depreciation=document.getElementById('depreciation');
-let selectedOption = 'Straight Line'
+const form = document.getElementById('form');
+const methods = document.getElementById('methods');
+const cost = document.getElementById('cost');
+const usefulLife = document.getElementById('usefulLife');
+const scrapValue = document.getElementById('scrapValue');
+const accDepreciation = document.getElementById('accDepreciation');
+const scrap = document.getElementById('scrap');
+const noOfUnits = document.getElementById('noOfUnits');
+const productionPerYear = document.getElementById('productionPerYear');
+const depreciation = document.getElementById('depreciation');
+let selectedOption = 'Straight Line';
 
-form.addEventListener('submit',(event)=>{
-    event.preventDefault();
-    let depreciationValue;
-    if(selectedOption==='Straight Line'){
-        const costValue=cost.value;
-        const scrapValueValue=scrapValue.value;
-        const usefulLifeValue=usefulLife.value
-        depreciationValue=(costValue-scrapValueValue)/usefulLifeValue
-    }else if(selectedOption === "Reducing Balance"){
-        const costValue=cost.value;
-        const accDepreciationValue=accDepreciation.value;
-        const usefulLifeValue=usefulLife.value
-        const scrapValue=scrap.value
-        depreciationValue=(costValue-accDepreciationValue)(1-Math.sqrt(scrapValue/usefulLifeValue))*100
-    }else{
-        const costValue=cost.value;
-        const scrapValue=scrap.value
-        const noOfUnitsValue=noOfUnits.value;
-        const productionPerYearValue= productionPerYear.value;
-        depreciationValue=(costValue-scrapValue)*productionPerYearValue/noOfUnitsValue;
-    }
-    depreciation.value=depreciationValue;
-})
+form.addEventListener('submit', (event) => {
+	event.preventDefault();
+	let depreciationValue;
+	if (selectedOption === 'Straight Line') {
+		const costValue = cost.value;
+		const scrapValueValue = scrapValue.value;
+		const usefulLifeValue = usefulLife.value;
+		depreciationValue = (costValue - scrapValueValue) / usefulLifeValue;
+	} else if (selectedOption === 'Reducing Balance') {
+		const costValue = cost.value;
+		const accDepreciationValue = accDepreciation.value;
+		const usefulLifeValue = usefulLife.value;
+		const scrapValue = scrap.value;
+		depreciationValue =
+			(costValue - accDepreciationValue)(1 - Math.sqrt(scrapValue / usefulLifeValue)) * 100;
+	} else {
+		const costValue = cost.value;
+		const scrapValue = scrap.value;
+		const noOfUnitsValue = noOfUnits.value;
+		const productionPerYearValue = productionPerYear.value;
+		depreciationValue = ((costValue - scrapValue) * productionPerYearValue) / noOfUnitsValue;
+	}
+	depreciation.value = depreciationValue;
+});
 
-methods.addEventListener('change',(event)=>{
-    selectedOption=event.target.value;
-    if(selectedOption==='Straight Line'){
-        //
-    }else if(selectedOption === "Reducing Balance"){
-        //
-    }else{
-        //
-    }
-})
+methods.addEventListener('change', (event) => {
+	selectedOption = event.target.value;
+	if (selectedOption === 'Straight Line') {
+		//
+	} else if (selectedOption === 'Reducing Balance') {
+		//
+	} else {
+		//
+	}
+});
